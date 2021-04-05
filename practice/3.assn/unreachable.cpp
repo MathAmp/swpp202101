ac903b739ed16cad993fca7ac297523678f0f880
mathamp <jwzz123123@gmail.com>
2021-04-05 11:11:42 +0900

    done before remove comments

--- practice/3.assn/unreachable.cpp
+++ practice/3.assn/unreachable.cpp
@@ -2,14 +2,66 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include<string>
+#include <string>
+#include <set>
+#include <queue>
+#include <map>
 using namespace llvm;
 
 namespace {
 class MyUnreachablePass : public PassInfoMixin<MyUnreachablePass> {
 public:
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    outs() << "UNIMPLEMENTED\n";
+    std::set<StringRef> unusedSet;
+    std::queue<StringRef> blockQueue;
+    std::map<StringRef, BasicBlock*> blockMap;
+    BasicBlock &entry = F.getEntryBlock();
+    blockQueue.push(entry.getName());
+
+    for (BasicBlock &BB : F) {
+      blockMap.insert(std::make_pair(BB.getName(), &BB));
+      unusedSet.insert(BB.getName());
+    }
+
+    while (!blockQueue.empty()) {
+      StringRef &s = blockQueue.front();
+      blockQueue.pop();
+      if (unusedSet.count(s)) {
+        unusedSet.erase(s);
+        BasicBlock &b = *blockMap[s];
+        for (Instruction &I : b) {
+          if (I.getOpcode() == Instruction::Br) {
+            for (unsigned int i = 0; i < I.getNumOperands(); i++) {
+              if (I.getNumOperands() == 1) {
+                blockQueue.push(I.getOperand(0)->getName());
+              } else {
+                blockQueue.push(I.getOperand(1)->getName());
+                blockQueue.push(I.getOperand(2)->getName());
+              }
+            }
+          }
+        }
+      }
+    }
+
+    for (StringRef s : unusedSet) {
+      outs() << s << "\n";
+    }
+
+    /*
+    outs() << "Entry : " << F.getEntryBlock().getName() << "\n";
+    outs() << "Front : " << F.front().getName() << "\n\n";
+    for (BasicBlock &BB : F) {
+      outs() << "Basic Block :" << BB.getName() << "\n";
+      for (Instruction &I : BB) {
+        for (unsigned int i = 0; i < I.getNumOperands(); i++) {
+          Value *v = I.getOperand(i);
+          outs() << "\t" << v->getValueID() << "\n";
+        }
+        outs() << "\t" << (I.getOpcode() == Instruction::Ret) << "\n";
+      }
+    }
+    */
     return PreservedAnalyses::all();
   }
 };
