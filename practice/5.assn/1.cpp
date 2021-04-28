9d1405b30ebd40ba3e1d7500e48b645ee397ca1b
mathamp <jwzz123123@gmail.com>
2021-04-28 23:37:49 +0900

    assn 5 init

---
+++ practice/5.assn/1.cpp
@@ -0,0 +1,185 @@
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <string>
+using namespace llvm;
+using namespace std;
+
+// This code save all domination tree for Edge(BranchInst->Next) to BasicBlock,
+// then
+// Please keep consistent indent rule.
+// I suggest that use more LLVM offering codes like Function &F and casting
+
+
+namespace {
+class PropagateIntegerEquality : public PassInfoMixin<PropagateIntegerEquality> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {\
+    // Do not TOO many use nested pair. Make another struct or class.
+	set<pair< BasicBlock *, pair<BasicBlock * , BasicBlock * > > > dom;
+	set<pair<pair<BasicBlock *, BasicBlock * > , BasicBlock * > > inv_dom;
+	vector<StringRef> inst;
+	vector<StringRef> arg;
+	// Use iterator (F.args())
+	for(auto a = F.arg_begin() ; a != F.arg_end() ; a++){
+		arg.push_back(a->getName());
+	}
+	DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+	// Elements of inv_dom differ only in order from dom's. One is unnecessary.
+	for(BasicBlock &BBstart : F){
+  		BranchInst *TI = dyn_cast<BranchInst>(BBstart.getTerminator());
+  		// Why left empty condition? Just use if (TI) {}
+      		if (!TI) {
+      		} else {
+			for(int i = 0 ; i < TI->getNumSuccessors() ; i++){
+          			BasicBlock *BBNext = TI->getSuccessor(i);
+          			BasicBlockEdge BBE(&BBstart, BBNext);
+   
+  	  			for (BasicBlock &BB : F) {
+          				if (DT.dominates(BBE, &BB)) {
+	      					dom.insert(make_pair(&BB, make_pair(&BBstart, BBNext)));
+						inv_dom.insert(make_pair(make_pair(&BBstart, BBNext) , &BB));
+					}
+          			}
+			}
+      		}
+	}
+
+	//
+	map<StringRef, pair<int, pair<Value * , Value * > > > icmp;
+	map<pair<BasicBlock * , BasicBlock * > , pair<Value *, Value *> > edge_pair;
+	for (auto &BB : F) {
+		for (auto &I : BB) {
+			switch (I.getOpcode()) {
+			case Instruction::ICmp:{
+				ICmpInst *IC = dyn_cast<ICmpInst>(&I);
+				// Predicate Num is hardcoded. Use internal constant like next
+				/* IC->getPredicate() == CmpInst::ICMP_EQ */
+				if(IC != NULL && (IC->getPredicate() == 32 /*|| IC->getPredicate() == 33*/)){
+					Value *V1 = I.getOperand(0);
+					Value *V2 = I.getOperand(1);
+					StringRef name1 = V1->getName();
+					StringRef name2 = V2->getName();
+					int index1, type1, index2, type2;
+					// Next two for loop can be abstracted with "Search" method
+					for(int i = 0 ; i < arg.size() ; i++){
+						if(arg[i] == name1){
+							type1 = 1;
+							index1 = i;
+						}
+						else if(arg[i] == name2){
+							type2 = 1;
+							index2 = i;
+						}
+					}
+					for(int i = 0 ; i < inst.size() ; i++){
+						if(inst[i] == name1){
+							type1 = 2;
+							index1 = i;
+						}
+						else if(inst[i] == name2){
+							type2 = 2;
+							index2 = i;
+						}
+					}	
+					if(type1 == type2){
+						if(index1 < index2) icmp.insert(make_pair(I.getName() , make_pair(IC->getPredicate(),make_pair(V2, V1))));
+						else icmp.insert(make_pair(I.getName(), make_pair(IC->getPredicate(), make_pair(V1, V2))));
+					} else{
+						if(type1 < type2) icmp.insert(make_pair(I.getName() , make_pair(IC->getPredicate(), make_pair(V2, V1))));
+						else icmp.insert(make_pair(I.getName(), make_pair(IC->getPredicate(), make_pair(V1, V2))));
+					}
+				}
+				break;
+	  		}
+	  		case Instruction::Br: {
+				if(I.getNumSuccessors() < 2)
+					break;
+				Value * V1 = I.getOperand(0);
+	        		map<StringRef, pair<int, pair<Value * , Value * > > >::iterator iter = icmp.find(V1->getName());
+				if(iter != icmp.end()){
+					Value *from = (iter->second).second.first;
+					Value *to = (iter->second).second.second;
+					int typ = (iter->second).first;
+					if(typ == 32){
+						BasicBlock * next = I.getSuccessor(0);
+						edge_pair.insert(make_pair(make_pair(&BB, next), make_pair(from, to)));
+					}
+					else if(typ == 33){
+						BasicBlock * next = I.getSuccessor(1);
+						edge_pair.insert(make_pair(make_pair(&BB, next), make_pair(from, to)));
+					}
+				}
+				break;
+	  		}
+          		default: {
+				inst.push_back(I.getName());
+                		break;
+	  		}
+			}
+		}
+	}
+    
+	for (auto &BB : F) {
+		for(auto dom_edge : dom){
+			if(dom_edge.first == &BB){
+				if(edge_pair.find(dom_edge.second) != edge_pair.end()){
+					Value *from = edge_pair.find(dom_edge.second)->second.first;
+					Value *to = edge_pair.find(dom_edge.second)->second.second;
+					from->replaceUsesWithIf(to, [&BB](Use &U) {
+					auto *In = dyn_cast<Instruction>(U.getUser());
+					return !In || In->getParent() == &BB;
+					});
+				}
+			}
+		}
+
+  		BranchInst *TI = dyn_cast<BranchInst>(BB.getTerminator());
+      		if (!TI) {
+      		} else {
+		for(int i = 0 ; i < TI->getNumSuccessors() ; i++){
+          		BasicBlock *BBNext = TI->getSuccessor(i);
+			for(auto dom_edge : inv_dom){
+				if(dom_edge.first == make_pair(&BB, BBNext)){
+					BasicBlock *target = dom_edge.second;
+					if(edge_pair.find(dom_edge.first) != edge_pair.end()){
+						Value *from = (edge_pair.find(dom_edge.first)->second).first;
+						Value *to = (edge_pair.find(dom_edge.first)->second).second;
+						from->replaceUsesWithIf(to, [target](Use &U) {
+						auto *In = dyn_cast<Instruction>(U.getUser());
+						return !In || In->getParent() == target;
+						});
+					}
+				}
+			}
+		}
+		}
+	}
+	return PreservedAnalyses::all();
+  }
+};
+}
+
+extern "C" ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "PropagateIntegerEquality", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+           ArrayRef<PassBuilder::PipelineElement>) {
+          if (Name == "prop-int-eq") {
+            FPM.addPass(PropagateIntegerEquality());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
---
+++ practice/5.assn/2.cpp
@@ -0,0 +1,83 @@
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/PatternMatch.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+using namespace std;
+using namespace llvm::PatternMatch;
+
+namespace {
+class PropagateIntegerEquality : public PassInfoMixin<PropagateIntegerEquality> {
+
+
+private:
+  // Replace the uses of b dominated by edge with a
+  void replace_operand(Value *a, Value *b, DominatorTree &DT, BasicBlockEdge &edge){
+    for (auto itr = b->use_begin(), end = b->use_end(); itr != end;){
+      Use &u = *itr++;
+      Instruction *userI = dyn_cast<Instruction>(u.getUser());
+      if (DT.dominates(edge, userI->getParent())){
+        u.set(a);
+      }
+    }
+  }
+
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        Value *x, *y;
+        ICmpInst::Predicate pred;
+        BasicBlock *bb_true, *bb_false;
+        if (match(&I, m_Br(m_ICmp(pred, m_Value(x), m_Value(y)), bb_true, bb_false)) && pred == ICmpInst::ICMP_EQ){
+          BasicBlockEdge edge(&BB, bb_true);
+          Instruction *i_x = dyn_cast<Instruction>(x);
+          Instruction *i_y = dyn_cast<Instruction>(y);
+
+          // x and y are both instructions -> replace the one which is dominated by the other
+          if (i_x && i_y){
+            if (DT.dominates(i_x,i_y)) replace_operand(x,y,DT, edge);
+            else replace_operand(y,x,DT, edge);
+          } else if (i_x){
+            replace_operand(y,x,DT, edge); //replace instruction x with arg y
+          } else if (i_y){
+            replace_operand(x,y,DT, edge); //replace insruction y with arg x
+          } else { // Replace the trailing argument
+            Argument *arg_x = dyn_cast<Argument>(x);
+            Argument *arg_y = dyn_cast<Argument>(y);
+            if (arg_x->getArgNo() < arg_y->getArgNo()){
+              replace_operand(x,y,DT, edge);
+            } else {
+              replace_operand(y,x,DT, edge);
+            }
+          }
+          break;
+        }
+      }
+    }
+    return PreservedAnalyses::all();
+  }
+};
+}
+
+extern "C" ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "PropagateIntegerEquality", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+           ArrayRef<PassBuilder::PipelineElement>) {
+          if (Name == "prop-int-eq") {
+            FPM.addPass(PropagateIntegerEquality());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
